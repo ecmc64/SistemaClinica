fba61b69137290d2ce9afe28754355c1a7021a7e
MDP07 <MDP07@PROYDIG02>
2017-11-17 19:52:30 -0500

    cambio about

---
+++ src/ProyectoClinica.Web/Models/PersonaModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoClinica.Web.Models
+{
+    public class PersonaModel
+    {
+        public string NroDocumento { get; set; }
+        public string Nombres { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public bool? Estado { get; set; }
+        public int PersonaId { get; set; }
+        public int? TipoDocumentoId { get; set; }
+    }
+}
